1e3612187bec0f0c6762eb8a051c1addcbd6955e
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2023-12-10 02:19:22 +0000

    add some debug logging

--- logger.py
+++ logger.py
@@ -2,7 +2,10 @@ import select
 from systemd import journal
 from bot import log_line
 
+print("one was run")
+
 def attach(service):
+    print("attaching now boss")
     j = journal.Reader()
     j.log_level(journal.LOG_INFO)
 
@@ -16,7 +19,10 @@ def attach(service):
     while p.poll():
         if j.process() != journal.APPEND:
             continue
+        
+        print("the condition is append!")
 
         for entry in j:
             if entry['MESSAGE'] != "":
+                print(str(entry['__REALTIME_TIMESTAMP'] )+ ' ' + entry['MESSAGE'])
                 log_line(str(entry['__REALTIME_TIMESTAMP'] )+ ' ' + entry['MESSAGE'])
