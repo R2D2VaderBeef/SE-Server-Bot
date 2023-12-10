288c7937a1edffb5f72173757b66854a2a868617
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2023-12-10 03:24:15 +0000

    screw abstraction

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import asyncio
 import discord
 import logger
 
+import select
+from systemd import journal
+
 channel = None
 
 class DiscordBot(discord.Client):
@@ -15,7 +18,28 @@ class DiscordBot(discord.Client):
         channel = client.get_channel(int(os.getenv('CHANNEL')))
         #logger_thread = threading.Thread(target=logger.attach, args=(log_line,))
         #logger_thread.start()
-        asyncio.run_coroutine_threadsafe(logger.attach(log_line), self.loop)
+        self.loop.create_task(self.attach())
+
+    def attach():
+        j = journal.Reader()
+        j.log_level(journal.LOG_INFO)
+
+        j.add_match(_SYSTEMD_UNIT=os.getenv('SERVICE'))
+        j.seek_tail()
+        j.get_previous()
+
+        p = select.poll()
+        p.register(j, j.get_events())
+
+        while p.poll():
+            if j.process() != journal.APPEND:
+                continue
+
+            for entry in j:
+                if entry['MESSAGE'] != "":
+                    #str(entry['__REALTIME_TIMESTAMP'] )+ ' ' +
+                    print(entry['MESSAGE'])
+                    asyncio.get_event_loop().create_task(log_line(entry['MESSAGE']))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,10 +48,7 @@ client = DiscordBot(intents=intents)
 def login():
     client.run(os.getenv('TOKEN'))
 
-def log_line(message):
-    asyncio.get_event_loop().create_task(send_log_message(message)) 
-
-async def send_log_message(message):
-    channel.send(message)
+async def log_line(message):
+    await channel.send(message) 
 
 login()
