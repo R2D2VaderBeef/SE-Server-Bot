d50b10cf0e6f2fa4f1c2ce6d90b6f4de2433c0ed
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2023-12-10 03:40:13 +0000

    screw the discord bot

--- main.py
+++ main.py
@@ -1,50 +1,24 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import asyncio
-
-import discord
 
 import select
 from systemd import journal
 
-channel = None
+j = journal.Reader()
+j.log_level(journal.LOG_INFO)
 
-class DiscordBot(discord.Client):
-    async def on_ready(self):
-        global channel
-        channel = client.get_channel(int(os.getenv('CHANNEL')))
-        self.loop.create_task(self.attach())
+j.add_match(_SYSTEMD_UNIT=os.getenv('SERVICE'))
+j.seek_tail()
+j.get_previous()
 
-    def attach(*args):
-        j = journal.Reader()
-        j.log_level(journal.LOG_INFO)
+p = select.poll()
+p.register(j, j.get_events())
 
-        j.add_match(_SYSTEMD_UNIT=os.getenv('SERVICE'))
-        j.seek_tail()
-        j.get_previous()
-
-        p = select.poll()
-        p.register(j, j.get_events())
-
-        while p.poll():
-            if j.process() != journal.APPEND:
-                continue
-
-            for entry in j:
-                if entry['MESSAGE'] != "":
-                    #str(entry['__REALTIME_TIMESTAMP'] )+ ' ' +
-                    print(entry['MESSAGE'])
-                    asyncio.get_event_loop().create_task(log_line(entry['MESSAGE']))
-
-intents = discord.Intents.default()
-intents.message_content = True
-client = DiscordBot(intents=intents)
-
-def login():
-    client.run(os.getenv('TOKEN'))
-
-async def log_line(message):
-    await channel.send(message) 
-
-login()
+while p.poll():
+    if j.process() != journal.APPEND:
+        continue
+    for entry in j:
+        if entry['MESSAGE'] != "":
+            #str(entry['__REALTIME_TIMESTAMP'] )+ ' ' +
+            print(entry['MESSAGE'])
